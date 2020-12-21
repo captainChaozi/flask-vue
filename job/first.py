0bc3828e634e0bda3b2190984f14b83c4672d825
chao <1095087479@qq.com>
2020-12-21 22:38:16 +0800

    优化 job部分 实现了job的分离

--- job/first.py
+++ job/first.py
@@ -1,8 +1,12 @@
-from app import get_session,apscheduler
+from app import get_session,apscheduler,redis
 from app.demo.model import Demo
 def job(a,b):
     with apscheduler.app.app_context():
         db_session = get_session()
         c = db_session.query(Demo)
         # print(c.all())
+        ss = redis.get('aa')
+        redis.set('aa',1)
+        s = redis.get('aa')
+        print(s)
     return a+b
